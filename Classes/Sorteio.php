71c22fa1b78994686336591000cc191dba6e8f22
Paulo Henrique <paulo.henrbraguim@gmail.com>
2021-06-05 00:32:37 -0300

    Segundo Commit

--- Classes/Sorteio.php
+++ Classes/Sorteio.php
@@ -1,7 +1,39 @@
 <?php
 
+require_once "Personagem.php";
+
 Class Sorteio{
 
-	
-	
+	private $pdo;
+	public $erro;
+
+	public function __construct(){
+
+		$databasename = 'dbsorteio';
+		$user = 'root';
+		$pass = 'root';
+		$host = 'localhost';
+
+		try{
+
+	    	$this->pdo = new PDO("mysql:dbname=".$databasename.";host=".$host, $user, $pass);
+
+		} catch(Exception $e){
+
+			$this->erro = $e->getMessage();
+
+		}	
+
+	}
+
+	public function Sortear(){
+
+		$p = new Personagem();
+
+		$p->carregarPersonagens();
+
+		
+
+	}
+
 }
